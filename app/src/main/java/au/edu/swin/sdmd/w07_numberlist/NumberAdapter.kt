7ba9e1666a70a8787a3bf64e4dc09dc8a08c5ef1
David Mackenzie <48002743+DavidAustinMackenzie@users.noreply.github.com>
2021-09-30 10:16:52 +1000

    Display numbers Display number details

--- app/src/main/java/au/edu/swin/sdmd/w07_numberlist/NumberAdapter.kt
+++ app/src/main/java/au/edu/swin/sdmd/w07_numberlist/NumberAdapter.kt
@@ -1,9 +1,11 @@
 package au.edu.swin.sdmd.w07_numberlist
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 
 class NumberAdapter(private val data: List<Int>) : RecyclerView.Adapter<NumberAdapter.ViewHolder>()  {
@@ -15,20 +17,25 @@ class NumberAdapter(private val data: List<Int>) : RecyclerView.Adapter<NumberAd
         return ViewHolder(view)
     }
 
-    override fun getItemCount() = 0
+    override fun getItemCount() = data.size
 
     override fun onBindViewHolder(holder: NumberAdapter.ViewHolder, position: Int) {
         val item = data[position]
         holder.bind(item)
     }
 
-    class ViewHolder(private val v: View) : RecyclerView.ViewHolder(v) {
+    inner class ViewHolder(private val v: View) : RecyclerView.ViewHolder(v) {
         private val number: TextView = v.findViewById(R.id.number)
 
+        //Setup the view in each row
         fun bind(item: Int) {
+            number.text = item.toString()
 
+            v.setOnClickListener{
+                val i = Intent(it.context,DetailActivity::class.java)
+                i.putExtra("NUMBER",item)
+                it.context.startActivity(i)
+            }
         }
-
     }
-
 }
